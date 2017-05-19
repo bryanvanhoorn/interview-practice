ddd40bda5b84d399a31f2b4e9c6f280872e5098b
Bryan Van Hoorn <bryan.vanhoorn@gmail.com>
2017-05-19 09:55:42 -0500

    implement simple string compression

--- src/main/java/org/interview/string/StringChecker.java
+++ src/main/java/org/interview/string/StringChecker.java
@@ -27,7 +27,7 @@ public class StringChecker {
         for (int i = 0; i < inputString.length(); i++) {
             char charToCheck = inputString.charAt(i);
 
-            for(int j = i +1; j < inputString.length(); j++) {
+            for (int j = i + 1; j < inputString.length(); j++) {
                 if (charToCheck == inputString.charAt(j)) {
                     return false;
                 }
@@ -50,7 +50,7 @@ public class StringChecker {
         Hashtable<Character, Integer> string1CharCounts = new Hashtable<Character, Integer>();
 
         char[] string1Array = inputString1.toCharArray();
-        for(int i = 0; i < string1Array.length; i++) {
+        for (int i = 0; i < string1Array.length; i++) {
             if (string1CharCounts.containsKey(string1Array[i])) {
                 int currentCount = string1CharCounts.get(string1Array[i]);
                 string1CharCounts.put(string1Array[i], currentCount++);
@@ -60,7 +60,7 @@ public class StringChecker {
         }
 
         char[] string2Array = inputString2.toCharArray();
-        for(int i = 0; i < string2Array.length; i++) {
+        for (int i = 0; i < string2Array.length; i++) {
             if (string1CharCounts.containsKey(string2Array[i])) {
                 int currentCount = string1CharCounts.get(string2Array[i]);
                 if (currentCount == 0) {
@@ -83,7 +83,7 @@ public class StringChecker {
         char[] inputStringArray = inputString.toCharArray();
 
         int numberOfSpaces = 0;
-        for(int i = 0; i < trueLength; i++) {
+        for (int i = 0; i < trueLength; i++) {
             if (inputStringArray[i] == ' ') {
                 numberOfSpaces++;
             }
@@ -91,8 +91,8 @@ public class StringChecker {
 
         int index = trueLength + (numberOfSpaces * 2);
 
-        for(int i = trueLength - 1; i >= 0; i--) {
-            if(inputStringArray[i] == ' ') {
+        for (int i = trueLength - 1; i >= 0; i--) {
+            if (inputStringArray[i] == ' ') {
                 inputStringArray[index - 1] = '0';
                 inputStringArray[index - 2] = '2';
                 inputStringArray[index - 3] = '%';
@@ -109,9 +109,9 @@ public class StringChecker {
     public static boolean isPermutationOfPalindrome(String inputString) {
         Map<Character, Integer> characterCounterMap = new HashMap<Character, Integer>();
         char[] inputStringArray = inputString.toCharArray();
-        for(int i = 0; i < inputStringArray.length; i++) {
+        for (int i = 0; i < inputStringArray.length; i++) {
             Character character = Character.valueOf(inputStringArray[i]);
-            if(characterCounterMap.containsKey(character)) {
+            if (characterCounterMap.containsKey(character)) {
                 Integer currentCount = characterCounterMap.get(character);
                 currentCount = currentCount + 1;
                 characterCounterMap.put(character, currentCount);
@@ -122,7 +122,7 @@ public class StringChecker {
 
         // if 0 or 1 letter have odd-numbered occurrences and all others are even, we're good
         int oddNumberedLetters = 0;
-        for(Integer count : characterCounterMap.values()) {
+        for (Integer count : characterCounterMap.values()) {
             if (count % 2 != 0) {
                 oddNumberedLetters++;
             }
@@ -149,7 +149,7 @@ public class StringChecker {
             // iterate char by char
             char[] inputString1Array = inputString1.toCharArray();
             char[] inputString2Array = inputString2.toCharArray();
-            for(int i = 0; i < inputString1Array.length; i++) {
+            for (int i = 0; i < inputString1Array.length; i++) {
                 if (inputString1Array[i] != inputString2Array[i]) {
                     numberOfDifferences++;
                 }
@@ -172,7 +172,7 @@ public class StringChecker {
             }
 
             int index1 = 0, index2 = 0;
-            while(index1 < shorterStringArray.length) {
+            while (index1 < shorterStringArray.length) {
                 if (shorterStringArray[index1] == longerStringArray[index2]) {
                     index1++;
                     index2++;
@@ -189,6 +189,31 @@ public class StringChecker {
         }
 
         return false;
+    }
+
+    public static String compressString(String inputString) {
+        char currentCharacter = inputString.charAt(0);
+        int currentCharacterCounter = 0;
+        StringBuilder outputStringBuilder = new StringBuilder();
+
+        for (int i = 0; i < inputString.length(); i++) {
+            if (inputString.charAt(i) == currentCharacter) {
+                currentCharacterCounter++;
+            } else {
+                outputStringBuilder.append(currentCharacter);
+                outputStringBuilder.append(currentCharacterCounter);
+                currentCharacter = inputString.charAt(i);
+                currentCharacterCounter = 1;
+            }
+        }
+        outputStringBuilder.append(currentCharacter);
+        outputStringBuilder.append(currentCharacterCounter);
+
+        if (outputStringBuilder.length() < inputString.length()) {
+            return outputStringBuilder.toString();
+        } else {
+            return inputString;
+        }
     }
 
 }
