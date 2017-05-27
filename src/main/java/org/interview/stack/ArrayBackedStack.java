67187291dfe1bfa93ca715873c2928439b88ef68
Bryan Van Hoorn <bryan.vanhoorn@gmail.com>
2017-05-26 21:29:57 -0500

    implement stack using array

---
+++ src/main/java/org/interview/stack/ArrayBackedStack.java
@@ -0,0 +1,35 @@
+package org.interview.stack;
+
+public class ArrayBackedStack {
+    private int[] stackData = new int[64];
+    private int topOfStackIndex = -1;
+
+    public int pop() {
+        if (topOfStackIndex < 0) {
+            throw new RuntimeException("nothing on the stack");
+        }
+
+        int popValue = stackData[topOfStackIndex];
+        topOfStackIndex--;
+        return popValue;
+    }
+
+    public void push(int newValue) {
+        topOfStackIndex++;
+        stackData[topOfStackIndex] = newValue;
+    }
+
+    public int peek() {
+        if (topOfStackIndex < 0) {
+            throw new RuntimeException("nothing on the stack");
+        }
+        return stackData[topOfStackIndex];
+    }
+
+    public boolean isEmpty() {
+        if (topOfStackIndex < 0) {
+            return true;
+        }
+        return false;
+    }
+}
