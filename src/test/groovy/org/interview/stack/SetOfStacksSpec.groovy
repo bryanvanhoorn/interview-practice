32a8f20b654bf18aed42f27f28f6d5c305e5e131
Bryan Van Hoorn <bryan.vanhoorn@gmail.com>
2017-05-28 20:30:10 -0500

    add popAt() method to pop an element from a specific stack

--- src/test/groovy/org/interview/stack/SetOfStacksSpec.groovy
+++ src/test/groovy/org/interview/stack/SetOfStacksSpec.groovy
@@ -4,15 +4,6 @@ import spock.lang.Specification
 
 class SetOfStacksSpec extends Specification {
 
-    /*
-Imagine a literal stack of plates.  If the stack gets too high, it might topple, so we would reasonably create
-another stack.  Implement a data structure SetOfStacks that mimics this.  Should be composed of several
-stacks and should create a new stack once the previous one exceeds capacity.  Push() and pop() should appear
-to have standard functionality.
-
-BONUS: Implement a function popAt(index) that performs a pop operation on a specific sub-stack.
- */
-
     def "should be able to push and pop on a set of stacks"() {
         given:
         SetOfStacks setOfStacks = new SetOfStacks()
@@ -41,7 +32,7 @@ BONUS: Implement a function popAt(index) that performs a pop operation on a spec
         assert setOfStacks.isEmpty() == true
     }
 
-    def "should be able to see that a new stack is added and removed when needed" () {
+    def "should be able to see that a new stack is added and removed when needed"() {
         given:
         SetOfStacks setOfStacks = new SetOfStacks()
 
@@ -50,23 +41,7 @@ BONUS: Implement a function popAt(index) that performs a pop operation on a spec
         assert setOfStacks.listOfStacks.size() == 1
 
         when:
-        setOfStacks.push(1)
-        setOfStacks.push(1)
-        setOfStacks.push(1)
-        setOfStacks.push(1)
-        setOfStacks.push(1)
-        setOfStacks.push(1)
-        setOfStacks.push(1)
-        setOfStacks.push(1)
-        setOfStacks.push(1)
-        setOfStacks.push(1)
-        setOfStacks.push(1)
-        setOfStacks.push(1)
-        setOfStacks.push(1)
-        setOfStacks.push(1)
-        setOfStacks.push(1)
-        setOfStacks.push(1)
-        setOfStacks.push(1)
+        addElementsToStack(setOfStacks)
 
         then:
         assert setOfStacks.listOfStacks.size() == 2
@@ -75,8 +50,86 @@ BONUS: Implement a function popAt(index) that performs a pop operation on a spec
         int poppedValue = setOfStacks.pop()
 
         then:
-        assert poppedValue == 1
+        assert poppedValue == 17
         assert setOfStacks.listOfStacks.size() == 1
     }
 
+    def addElementsToStack(SetOfStacks setOfStacks) {
+        setOfStacks.push(1)
+        setOfStacks.push(2)
+        setOfStacks.push(3)
+        setOfStacks.push(4)
+        setOfStacks.push(5)
+        setOfStacks.push(6)
+        setOfStacks.push(7)
+        setOfStacks.push(8)
+        setOfStacks.push(9)
+        setOfStacks.push(10)
+        setOfStacks.push(11)
+        setOfStacks.push(12)
+        setOfStacks.push(13)
+        setOfStacks.push(14)
+        setOfStacks.push(15)
+        setOfStacks.push(16)
+        setOfStacks.push(17)
+    }
+
+    def "should be able to pop a value from a specific sub-stack"() {
+        given:
+        SetOfStacks setOfStacks = new SetOfStacks()
+
+        expect:
+        assert setOfStacks.isEmpty() == true
+        assert setOfStacks.listOfStacks.size() == 1
+
+        when:
+        addElementsToStack(setOfStacks)
+
+        then:
+        assert setOfStacks.listOfStacks.size() == 2
+
+        when:
+        int poppedValue = setOfStacks.popAt(0)
+
+        then:
+        assert poppedValue == 16
+    }
+
+    def "popAt() should throw an exception if we try to pop an index that doesn't exist"() {
+        given:
+        SetOfStacks setOfStacks = new SetOfStacks()
+
+        when:
+        setOfStacks.popAt(2)
+
+        then:
+        thrown(RuntimeException)
+    }
+
+    def "popAt() should cause all elements to be shifted down to fill in the gap"() {
+        given:
+        SetOfStacks setOfStacks = new SetOfStacks()
+
+        expect:
+        assert setOfStacks.isEmpty() == true
+        assert setOfStacks.listOfStacks.size() == 1
+
+        when:
+        addElementsToStack(setOfStacks)
+
+        then:
+        assert setOfStacks.listOfStacks.size() == 2
+
+        when:
+        int poppedValue = setOfStacks.popAt(0)
+
+        then:
+        assert poppedValue == 16
+
+        when:
+        poppedValue = setOfStacks.popAt(0)
+
+        then:
+        assert poppedValue == 17
+    }
 }
