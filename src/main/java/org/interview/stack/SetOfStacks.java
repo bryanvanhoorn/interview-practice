b0f36f6fc18f12fc204d07de33be5bcb6d3e65f0
Bryan Van Hoorn <bryan.vanhoorn@gmail.com>
2017-05-28 20:30:10 -0500

    add popAt() method to pop an element from a specific stack

--- src/main/java/org/interview/stack/SetOfStacks.java
+++ src/main/java/org/interview/stack/SetOfStacks.java
@@ -1,5 +1,6 @@
 package org.interview.stack;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -33,7 +34,52 @@ public class SetOfStacks {
         return poppedValue;
     }
 
-    public int peek () {
+    public int popAt(int stackIndex) {
+        if (stackIndex >= listOfStacks.size()) {
+            throw new RuntimeException("no stack at index " + stackIndex);
+        }
+
+        ArrayBackedStack arrayBackedStack = listOfStacks.get(stackIndex);
+        int poppedValue = arrayBackedStack.pop();
+
+        shiftAllElementsDown(stackIndex);
+
+        if (arrayBackedStack.isEmpty()) {
+            listOfStacks.remove(stackIndex);
+        }
+
+        return poppedValue;
+    }
+
+    private void shiftAllElementsDown(int startingStackIndex) {
+        ArrayBackedStack currentStack = listOfStacks.get(startingStackIndex);
+        if (currentStack.isFull() == false) {
+            // check for next stack
+            if (startingStackIndex + 1 < listOfStacks.size()) {
+                ArrayBackedStack nextStack = listOfStacks.get(startingStackIndex + 1);
+
+                // pop all values onto another stack until empty
+                ArrayBackedStack tempStack = new ArrayBackedStack();
+                while (nextStack.isEmpty() == false) {
+                    tempStack.push(nextStack.pop());
+                }
+
+                // move 0th node to the current stack
+                if (tempStack.isEmpty() == false) {
+                    currentStack.push(tempStack.pop());
+                }
+
+                // push all nodes back onto next stack
+                while (tempStack.isEmpty() == false) {
+                    nextStack.push(tempStack.pop());
+                }
+
+                shiftAllElementsDown(startingStackIndex + 1);
+            }
+        }
+    }
+
+    public int peek() {
         ArrayBackedStack latestStack = listOfStacks.get(listOfStacks.size() - 1);
         return latestStack.peek();
     }
